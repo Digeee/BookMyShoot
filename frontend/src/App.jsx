d1f07f4c7ff00d9742ef4a8a04567342e118ea4d
Jeganathan Digevan <digee12@gmail.com>
2025-12-05 15:08:04 +0530

    ui changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen font-primary">
+      <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
           <Routes>
